d5a3900dd0e5669748bc7aa7631b612ad072dc03
Chris Korbel <christopher.korbel@liferay.com>
2018-01-24 17:34:27 -0800

    added webpack --time to modularize

--- src/sandbox/counter-example.js
+++ src/sandbox/counter-example.js
@@ -1,13 +1,30 @@
 class Counter extends React.Component {
     constructor(props) {
         super(props);
-    this.handleAddOne = this.handleAddOne.bind(this);
-    this.handleMinusOne = this.handleMinusOne.bind(this);
-    this.handleReset = this.handleReset.bind(this);
-    this.state = {
-        count: 0
-    };
+        this.handleAddOne = this.handleAddOne.bind(this);
+        this.handleMinusOne = this.handleMinusOne.bind(this);
+        this.handleReset = this.handleReset.bind(this);
+        this.state = {
+            count: 0
+        };
     }
+
+    componentDidMount() {
+        const stringCount = localStorage.getItem('count');
+        const count = parseInt(stringCount, 10);   
+
+        if (!isNaN(count)) {
+            this.setState(() => ({ count }));
+        }
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if(prevState.count !== this.state.count) {
+            localStorage.setItem('count', this.state.count);
+        }
+    }
+
+
     handleAddOne() {
         console.log('hit add one');
         this.setState((prevState)=> {
@@ -37,6 +54,7 @@ class Counter extends React.Component {
     render() {
         return (
             <div>
+                <h1>Count: {this.state.count}</h1>
                 <button onClick={this.handleAddOne}>+1</button>
                 <button onClick={this.handleMinusOne}>-1</button>
                 <button onClick={this.handleReset}>reset</button>
@@ -44,6 +62,10 @@ class Counter extends React.Component {
         );
     }
 }
+
+
+ReactDOM.render(<Counter />, document.getElementById('app'))
+
 
 
 // let count = 0;
