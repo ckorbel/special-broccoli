9f60bf17d96541f3bc98d767f6d97202f29f8eda
Chris Korbel <christopher.korbel@liferay.com>
2018-01-29 09:20:46 -0800

    added react-router

--- src/app.js
+++ src/app.js
@@ -1,7 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss'; 
 
+const ExpenseDashboardPage = () => (
+    <div>
+        This is from dashboard component
+    </div>
+);
 
-ReactDOM.render(<p>React Biolerplate</p>, document.getElementById('app'));
+const AddExpensePage = () => (
+    <div>
+        This is from the AddExpensePage.
+    </div>
+);
+
+const EditExpensePage = () => (
+    <div>
+        This is from the EditExpensePage.
+    </div>
+);
+
+const HelpPage = () => (
+    <div>
+        This is from the HelpPage.
+    </div>
+);
+
+const routes = (
+    <BrowserRouter>
+        <div>
+        <Route path="/" component={ExpenseDashboardPage} exact={true} />
+        <Route path="/create" component={AddExpensePage} />
+        <Route path="/edit" component={EditExpensePage}/>
+        <Route path="/help" component={HelpPage}/>
+        </div>
+        
+    </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
